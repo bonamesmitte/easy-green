42c64e4aed98953b029cd6104ce1f4300988e2d3
STUDENT_M16 <student_m16@PS-DAL-M16.local>
2020-09-24 18:38:45 -0500

    Updates

---
+++ precache-manifest.916d5e515e67567b05ab1eadad9e56cf.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bc44d7d5bf00460caa638c9f1b1bd48c",
+    "url": "/easy-green/index.html"
+  },
+  {
+    "revision": "104ca3c15d4c058ce928",
+    "url": "/easy-green/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "e18858daecd8d702ba8e",
+    "url": "/easy-green/static/css/main.380ece2d.chunk.css"
+  },
+  {
+    "revision": "104ca3c15d4c058ce928",
+    "url": "/easy-green/static/js/2.c800cbd2.chunk.js"
+  },
+  {
+    "revision": "6b3248a1692933689dcb27b9e1ed5092",
+    "url": "/easy-green/static/js/2.c800cbd2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e18858daecd8d702ba8e",
+    "url": "/easy-green/static/js/main.bbe65979.chunk.js"
+  },
+  {
+    "revision": "00e76535a45dfc132b1c",
+    "url": "/easy-green/static/js/runtime-main.7be1764b.js"
+  },
+  {
+    "revision": "2bd28345ff3d519f2883de5bc2169319",
+    "url": "/easy-green/static/media/avatar.2bd28345.png"
+  },
+  {
+    "revision": "e4c34bbd933e705a8438dc6dec006162",
+    "url": "/easy-green/static/media/management.e4c34bbd.png"
+  }
+]);
